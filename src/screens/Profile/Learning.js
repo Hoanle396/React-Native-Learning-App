6dbd26478b2255399374c80e4e8a53d290711068
Lê Hữu Hoàn <hoanle396@gmail.com>
2023-06-11 23:32:04 +0700

    update player

--- src/screens/Profile/Learning.js
+++ src/screens/Profile/Learning.js
@@ -10,7 +10,7 @@ export default function Learning({ route, navigation }) {
       <View style={{ width: '100%', height: '100%', paddingTop: 40, backgroundColor: "#FFFFEE" }}>
          <VideoPlayer
             videoProps={{
-               shouldPlay: false,
+               shouldPlay: true,
                source: {
                   uri: state,
                },
